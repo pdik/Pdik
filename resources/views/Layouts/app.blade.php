ec8c7bff999ae4f43f341aa24f1283253a7f3f73
pepijn <pepijn@pdik.nl>
2021-01-25 11:44:49 +0100

    Updated Readme.md, And 3 blade templates, Updated composer.json

---
+++ resources/views/Layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+
+    <title>Pdik saas</title>
+    <meta name="description" content="Saas application Pdik systems">
+    <meta name="author" content="Pdik">
+    @livewireStyles
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
+
+@livewireScripts
+<script src="js/scripts.js"></script>
+</body>
+</html>
