d6dd2b5bcdf150c50c773dea5f1a3b9c5f4ebf51
pepijn <pepijn@pdik.nl>
2021-01-25 12:39:59 +0100

    Making everyting Tentant ready :-) Basics for Saas appplication

---
+++ app/Models/Tentant.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\DatabaseConfig;
+
+class Tentant extends BaseTenant implements TenantWithDatabase
+{
+    use HasDatabase, HasDomains;
+
+}
